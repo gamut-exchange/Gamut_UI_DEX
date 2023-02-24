02738ce4b21fa306b14b56211e2d29c11b4226ac
truefrog <superstar200720@gmail.com>
2023-02-24 07:39:54 -0600

    fixed for httplink

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,16 +1,6 @@
-import { ApolloClient, InMemoryCache, HttpLink, Hawk, credentials } from '@apollo/client'
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 
-const customFetch = (uri, options) => {
-  const { header } = Hawk.client.header(
-    "http://65.109.205.22:8000/subgraphs/name/gamut-subgraph-kava",
-    "POST",
-    { credentials: credentials, ext: "some-app-data" }
-  );
-  options.headers.Authorization = header;
-  return fetch(uri, options);
-};
-
-const link = new HttpLink({ fetch: customFetch });
+const link = new HttpLink({ uri:"http://65.109.205.22:8000/subgraphs/name/gamut-subgraph-kava" });
 
 export const kavaClient = new ApolloClient({
   link: link,
