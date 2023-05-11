cb9f90019c42e2109545eb06cb25b1cde1dbeeae
truefrog <superstar200720@gmail.com>
2023-05-11 01:53:33 -0500

    add cross-chain iframe

--- src/components/views/CrossChain.js
+++ src/components/views/CrossChain.js
@@ -1,6 +1,5 @@
 
 import React, { useEffect, useState } from "react";
-import { SquidWidget } from "@0xsquid/widget";
 import { useWeb3React } from "@web3-react/core";
 import {
     Grid,
@@ -103,57 +102,13 @@ export default function CrossChain() {
                         </Stack>
                     </Item>
                 </Grid>
-                <Grid item={true} xs={12} sx={{ p: 1, display: "flex", justifyContent: "center" }}>
-                    <SquidWidget style={{ display:"flex", justifyContent:"center" }} config={
-                        {
-                            "companyName": "Gamut",
-                            "style": {
-                                "neutralContent": "#959BB2",
-                                "baseContent": "#E8ECF2",
-                                "base100": "#07071c",
-                                "base200": "#121226",
-                                "base300": "#171D2B",
-                                "error": "#ED6A5E",
-                                "warning": "#FFB155",
-                                "success": "#62C555",
-                                "primary": "#0583f4",
-                                "secondary": "#0583f4",
-                                "secondaryContent": "#191C29",
-                                "neutral": "#12122c",
-                                "roundedBtn": "5px",
-                                "roundedBox": "5px",
-                                "roundedDropDown": "7px",
-                                "displayDivider": true
-                            },
-                            "mainLogoUrl": "/logo-bow.svg",
-                            "slippage": 1.5,
-                            "infiniteApproval": false,
-                            "enableExpress": false,
-                            "apiUrl": "https://api.squidrouter.com",
-                            "comingSoonChainIds": [
-                                "cosmoshub-4",
-                                "injective-1",
-                                "axelar-dojo-1",
-                                "kichain-2"
-                            ],
-                            "titles": {
-                                "swap": "Convert",
-                                "settings": "Settings",
-                                "wallets": "Wallets",
-                                "tokens": "Tokens",
-                                "chains": "Chains",
-                                "history": "History",
-                                "transaction": "Transaction",
-                                "allTokens": "Tokens",
-                                "destination": "Destination address"
-                            },
-                            "priceImpactWarnings": {
-                                "warning": 3,
-                                "critical": 5
-                            }
-                        }
-                    } />
-
+                <Grid item={true} xs={12} sx={{ p: 1, display: "flex" }}>
+                    <iframe
+                        title="squid_widget"
+                        width="420"
+                        height="641"
+                        src="https://widget.squidrouter.com/iframe?config=%7B%22companyName%22%3A%22Gamut%22%2C%22style%22%3A%7B%22neutralContent%22%3A%22%23959BB2%22%2C%22baseContent%22%3A%22%23E8ECF2%22%2C%22base100%22%3A%22%2307071C%22%2C%22base200%22%3A%22%23121226%22%2C%22base300%22%3A%22%23171D2B%22%2C%22error%22%3A%22%23ED6A5E%22%2C%22warning%22%3A%22%23FFB155%22%2C%22success%22%3A%22%2362C555%22%2C%22primary%22%3A%22%230583F4%22%2C%22secondary%22%3A%22%230583F4%22%2C%22secondaryContent%22%3A%22%23191C29%22%2C%22neutral%22%3A%22%2312122C%22%2C%22roundedBtn%22%3A%225px%22%2C%22roundedBox%22%3A%225px%22%2C%22roundedDropDown%22%3A%227px%22%2C%22displayDivider%22%3Atrue%7D%2C%22slippage%22%3A1.5%2C%22infiniteApproval%22%3Afalse%2C%22enableExpress%22%3Afalse%2C%22apiUrl%22%3A%22https%3A%2F%2Fapi.squidrouter.com%22%2C%22comingSoonChainIds%22%3A%5B%22cosmoshub-4%22%2C%22injective-1%22%2C%22axelar-dojo-1%22%2C%22kichain-2%22%5D%2C%22titles%22%3A%7B%22swap%22%3A%22Convert%22%2C%22settings%22%3A%22Settings%22%2C%22wallets%22%3A%22Wallets%22%2C%22tokens%22%3A%22Tokens%22%2C%22chains%22%3A%22Chains%22%2C%22history%22%3A%22History%22%2C%22transaction%22%3A%22Transaction%22%2C%22allTokens%22%3A%22Tokens%22%2C%22destination%22%3A%22Destination%20address%22%7D%2C%22priceImpactWarnings%22%3A%7B%22warning%22%3A3%2C%22critical%22%3A5%7D%7D"
+                    />
                 </Grid>
             </Grid>
         </div>
